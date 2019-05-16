be4f54c20adf05d470086dc81d40a00000b753b5
Snugglez <32309975+Snugglez@users.noreply.github.com>
2019-05-16 07:52:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = function reee(d) {
 d.hook('S_LOGIN', 13, (e) => {
-d.log(e)
 if(d.game.me.class == 'fighter'){
 e.templateId = 10911
 e.appearance = {unk: 101, skinColor: 12, faceStyle: 8, faceDecal: 3, hairColor: 1, tattoos: 0 }
